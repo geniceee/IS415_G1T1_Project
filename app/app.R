22927ef0803ce50515dfa39d5af3272cf9ea28b5
wlwong2018 <wlwong.2018@sis.smu.edu.sg>
2021-11-09 15:14:14 +0800

    EDA - choropleth map

--- app/app.R
+++ app/app.R
@@ -15,6 +15,8 @@ library(tmap)
 library(reshape2)
 library(sp)
 library(caret)
+library(shinythemes)
+library(shinyjs)
 
 ## 4.1 Import Geospatial data
 mpsz_sf <- st_read(dsn = "data/geospatial", layer="MP14_SUBZONE_WEB_PL")
@@ -47,47 +49,157 @@ print(od_pop_sz)
 
 ### Configuration ###
 ui <- fluidPage(
-    # Choropleth Mapping - Reactive
-    titlePanel("Choropleth Mapping - Reactive"),
-    sidebarLayout(
-        sidebarPanel(
-            selectInput(inputId = "colour",
-                        label = "Colour of Points",
-                        choices = c("blue" = "blue",
-                                    "green" = "green",
-                                    "red" = "red"),
-                        selected = "blue"),
+    
+    tabPanel("EDA", fluid = TRUE),
+        sidebarLayout(
+            sidebarPanel(
+                
+                # Spatial Point Map of Bus Stops
+                conditionalPanel(
+                    'input.EDA_var === "Spatial Points"',
+                    selectInput(inputId = "colour_pmap",
+                                label = "Colour of Points",
+                                choices = c("blue" = "blue",
+                                            "green" = "green",
+                                            "red" = "red"),
+                                selected = "blue"),
+                    
+                    sliderInput(inputId = "alpha",
+                                label = "Transparency of Points",
+                                min = 0.3,
+                                max = 1,
+                                value = c(0.4)),
+                    sliderInput(inputId = "size",
+                                label = "Size of Points",
+                                min = 0.01,
+                                max = 0.1,
+                                value = c(0.03))
+                ), # 1st conditionalPanel
+                
+                # Choropleth
+                conditionalPanel(
+                    'input.EDA_var === "Choropleth Maps',
+                    selectInput(inputId = "classification",
+                                label = "Classification method",
+                                choices = c("fixed" = "fixed",
+                                               "sd" = "sd",
+                                               "equal" = "equal",
+                                               "pretty" = "pretty",
+                                               "quantile" = "quantile",
+                                               "kmeans" = "kmeans",
+                                               "hclust" = "hclust",
+                                               "bclust" = "bclust",
+                                               "fisher" = "fisher",
+                                               "jenks" = "jenks"),
+                                selected = "pretty"), 
+                    selectInput(inputId = "colour_cmap",
+                                label = "Colour scheme",
+                                choices = c("blues" = "Blues",
+                                               "reds" = "Reds",
+                                               "greens" = "Greens",
+                                               "Yellow-Orange-Red" = "YlOrRd",
+                                               "Yellow-Orange-Brown" = "YlOrBr",
+                                               "Yellow-Green" = "YlGn",
+                                               "Orange-Red" = "OrRd"),
+                                selected = "YlOrRd"),
+                    selectInput(inputId = "o_d",
+                                label = "Origin/Destination:",
+                                choices = c("Origin" = "ORIGIN_SZ_C",
+                                               "Destination" = "DEST_SZ_C"),
+                                selected = "ORIGIN_SZ_C"),
+                    
+                    selectInput(inputId = "dt",
+                                label = "Day Type:",
+                                choices = c("Weekday" = "WEEKDAY",
+                                               "Weekend" = "WEEKENDS/HOLIDAY"),
+                                selected = "WEEKDAY"),
+                    
+                    selectInput(inputId = "hr_mth",
+                                label = "Hour/Month Interval:",
+                                choices = c("Month" = "YEAR_MONTH",
+                                             "Hour" = "TIME_PER_HOUR"),
+                                selected = "YEAR_MONTH")
+                    
+                ) # 2nd conditionalPanel
+                
+            ), #sidebarPanel
+                
             
-            sliderInput(inputId = "alpha",
-                        label = "Transparency of Points",
-                        min = 0.3,
-                        max = 1,
-                        value = c(0.4)),
-            sliderInput(inputId = "size",
-                        label = "Size of Points",
-                        min = 0.01,
-                        max = 0.1,
-                        value = c(0.03))
-        ),
-        mainPanel(
-            tmapOutput("pt_bus_stop"),
-            # DT::dataTableOutput(outputId = "aTable")
-        )
-        
-        
-    )
-)
+            mainPanel(
+                tabsetPanel(
+                    id="EDA_var",
+                    tabPanel("Spatial Points", tmapOutput("pt_bus_stop")),
+                    # DT::dataTableOutput(outputId = "aTable") 
+                    tabPanel("Choropleth Maps", plotOutput(outputId =  "cmap"))
+                )
+
+            )
+            
+            
+    )#sidebarLayout
+    
+)#fluidpage
+
 
 
 
 server <- function(input, output){
+    # Spatial Points Map
     output$pt_bus_stop <- renderTmap({
         tm_shape(mpsz_sf) +
             tm_polygons() +
             tm_shape(bus_sf) +
             tm_dots(alpha = input$alpha,
-                    col = input$colour,
+                    col = input$colour_pmap,
                     size = input$size)
+    })
+    
+    # Choropleth
+    output$cmap <- renderPlot({
+        
+        sub_df <- od_pop_sz %>% filter(DAY_TYPE==input$dt)%>% 
+                                group_by(!!!syms(input$o_d), !!!syms(input$hr_mth))  %>%
+                                summarise(TOTAL_TRIPS = sum(TRIPS))  %>%
+                                ungroup()  %>%
+                                select(input$o_d, input$hr_mth, TOTAL_TRIPS)  %>%
+                                pivot_wider(names_from=input$hr_mth,  values_from=TOTAL_TRIPS)
+        
+        # Replace NA values with 0
+        sub_df[is.na(sub_df)] = 0
+
+        # Get unique values of grp_by value
+        unique_grp <- sort(unique(od_pop_sz[[input$hr_mth]]))
+
+        # if categorical variable is numeric, change to character only after sorting
+        if (input$hr_mth=="TIME_PER_HOUR"){
+            unique_grp <- as.character(sort(as.numeric(unique_grp)))
+        }
+
+        # Perform left join
+        mpsz_od_grp <- left_join(mpsz_sf, sub_df, by = c('SUBZONE_C' = input$o_d))
+
+        # Create list to store choropleth maps
+        cmap_list <- vector(mode = "list", length = length(unique_grp))
+        
+        for (i in 1:length(unique_grp)) {
+            cmap <- tm_shape(mpsz_od_grp) +
+                tm_fill(col = unique_grp[[i]],
+                        palette = input$colour_cmap,
+                        style = input$classification,
+                        n = 5) +
+                tm_borders(lwd = 0.05,
+                           alpha = 0.5) +
+
+                tm_layout(panel.show = TRUE,
+                          panel.labels = unique_grp[[i]],
+                          panel.label.color = 'black',
+                          panel.label.size = 0.7,
+                          inner.margins = 0,
+                          legend.text.size = 0.4,
+                          frame=T)
+            cmap_list[[i]] <- cmap
+        }
+        do.call(tmap_arrange, cmap_list)
     })
     
 }
